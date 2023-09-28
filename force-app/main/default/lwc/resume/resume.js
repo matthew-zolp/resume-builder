85a3f9f3ebf66ba3d1619755b7f88828a408e15e
Matt Zolp <Zolp_Matthew@bah.com>
2023-09-28 12:12:08 -0500

    save

--- force-app/main/default/lwc/resume/resume.js
+++ force-app/main/default/lwc/resume/resume.js
@@ -1,8 +1,25 @@
-import { LightningElement } from 'lwc';
-
+import { LightningElement, track } from 'lwc';
+import getResume from "@salesforce/apex/ResumeBuilderController.getResume";
 export default class Resume extends LightningElement {
+    @track resume;
+    @track loading;
+    connectedCallback(){
+        this.getResumeJS();
+        console.log('We are live!');
+    }
 
-    
+    getResumeJS(){
+        this.loading = true;
+        try {
+            this.resume = getResume();
+            console.log('ResumeJSON: ' + JSON.stringify(this.resume));
+        } catch (e) {
+        }
+        finally {
+            this.loading = false;
+        }
+    }
+
 
 
 
